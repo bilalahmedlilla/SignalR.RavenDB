eae8a01a96eae6243a7b3ab761e6dea0ef9421ca
Bilal Ahmed <bilal.ahmed.lilla@gmail.com>
2023-08-02 12:46:26 +0500

    Add files via upload
    update signalR.Raven backplane using raven.client 5.4 latest

---
+++ RavenMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR.Messaging;
+
+namespace SignalR.RavenDB
+{
+    public class RavenMessage
+    {
+        public string Id { get; set; }
+
+        public int StreamIndex { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public static RavenMessage FromMessages(int streamIndex, IList<Message> messages)
+        {
+            var scaleoutMessage = new ScaleoutMessage(messages);
+            return new RavenMessage
+            {
+                StreamIndex = streamIndex,
+                Data = scaleoutMessage.ToBytes()
+            };
+        }
+
+        public ScaleoutMessage ToScaleoutMessage()
+        {
+            return ScaleoutMessage.FromBytes(this.Data);
+        }
+
+        public ulong ToLongId()
+        {
+            return Convert.ToUInt64(this.Id.Substring(this.Id.LastIndexOf('/') + 1));
+        }
+    }    
+}
